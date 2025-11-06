f643e4ab7ba4f550c5af7b7026780f23fc3eb3e8
GomathinayagamS <carthysgn@gmail.com>
2025-11-06 16:34:02 -0700

    Update pp_script.sh

--- pp_script.sh
+++ pp_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #conda activate prodigal_gv
-# Check if the nucleotide file is provided as a command-line argument
+
 if [ $# -eq 0 ]; then
     echo "Please provide the nucleotide file as a command-line argument."
     exit 1
@@ -13,22 +13,22 @@ HMM_FILE="/home/sgomathi/phage_plasmids/essentials/merged_plasmid_hallmark.hmm"
 
 seqkit seq -m 3000 $1 > NUCLEOTIDE_FILE_fil.fa &&
 
-# Step 1: Call ORFs using Prodigal-gv
+# Prodigal-gv
 /home/sgomathi/phage_plasmids/essentials/./prodigal-gv -p meta -i NUCLEOTIDE_FILE_fil.fa -a output_proteins.faa -q
 
-# Step 2: Search for hits using hmmsearch
+# hmmsearch
 hmmsearch -E 1e-20 --tblout new_output_hits.tblout "$HMM_FILE" output_proteins.faa
 
-# Step 3: Retrieve hits using awk
+# Retrieve hits 
 awk '{print $1}' new_output_hits.tblout | grep -o '.*_' - > new_list_of_hits.txt
 grep -o '.*[^_]' new_list_of_hits.txt > new_hits.txt
 
-# Step 4: Retrieve hits.fna using grep
+# Retrieve hits.fna 
 seqtk subseq "$NUCLEOTIDE_FILE" new_hits.txt > tmp.fna
 seqkit rmdup -s tmp.fna > new_hits.fna &&
 rm tmp.fna 
 
 awk '{OFS="\t"; print $1, $3, $4}' new_output_hits.tblout | grep -o '.*[^_]' - > new_list_of_hits.txt
 
-# Step 5: QC of hits.fna using CheckV
+# CheckV
 checkv end_to_end new_hits.fna checkv_out -d $HOME/checkv-db-v1.5 -t 26
